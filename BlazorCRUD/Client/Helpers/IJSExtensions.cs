6c29c566ff10e4b3789d4818ed07c714b80f7d33
KendallSalmeron <kensalmeron@gmail.com>
2019-08-21 17:02:01 -0600

    cruds y exports a excel

---
+++ BlazorCRUD/Client/Helpers/IJSExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorCRUD.Client.Helpers
+{
+    public static class IJSExtensions
+    {
+        public static Task GuadarComo(this Microsoft.JSInterop.IJSRuntime js, string nombreArchivo, byte[] archivo)
+        {
+            return js.InvokeAsync<object>("saveAsFile", 
+                nombreArchivo,
+                Convert.ToBase64String(archivo));
+        
+        }
+    }
+}
--- BlazorCRUD/Server/Controllers/PersonasController.cs
+++ BlazorCRUD/Server/Controllers/PersonasController.cs
@@ -24,6 +24,37 @@ namespace BlazorCRUD.Server.Controllers
         {
             return await context.Personas.ToListAsync();
         }
+
+        [HttpGet("{id}", Name = "obtenerPersona")]
+        public async Task<ActionResult<Persona>> Get(int id)
+        {
+            return await context.Personas.FirstOrDefaultAsync(x => x.ID == id);
+
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Persona persona)
+        {
+            context.Add(persona);
+            await context.SaveChangesAsync();
+            return new CreatedAtRouteResult("obtenerPersona", new { id = persona.ID }, persona);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> Put(Persona persona)
+        {
+            context.Entry(persona).State = EntityState.Modified;
+            await context.SaveChangesAsync();
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var persona = new Persona { ID = id };
+            context.Remove(persona);
+            await context.SaveChangesAsync();
+            return NoContent();
+        }
        
     }
 }
